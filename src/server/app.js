7aeee278c6645d5f61d75e9e07cdeb32c177091f
arun <arunkumar413@gmail.com>
2023-03-03 19:44:17 +0530

    save

---
+++ src/server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static("dist"));
+
+// app.use(express.static(path.join(__dirname, "dist")));
+
+app.use(express.static(path.join(__dirname, "../..", "dist")));
+
+app.use(express.static("dist"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../..", "dist", "index.html"));
+});
+
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
