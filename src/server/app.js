646c6aadcac86f0f3ade80ee8cb6c36ff1988a23
arun <arunkumar413@gmail.com>
2023-03-03 21:06:39 +0530

    save

--- src/server/app.js
+++ src/server/app.js
@@ -7,12 +7,12 @@ const port = 3000;
 
 // app.use(express.static(path.join(__dirname, "dist")));
 
-app.use(express.static(path.join(__dirname, "../..", "dist")));
+app.use(express.static(path.join(__dirname, "../client", "dist")));
 
 app.use(express.static("dist"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../..", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../../client", "dist", "index.html"));
 });
 
 app.get("/api", (req, res) => {
